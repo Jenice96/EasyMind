8065b16d0f98f69fed025d3c7fcf813bdb2fb207
Jenice96 <2861806421@qq.com>
2017-05-24 00:36:27 +0800

    add about&main
    添加了关于、主界面的toolbar部分。

---
+++ app/src/main/java/cn/studyjams/s2/sj175/jenice/AboutActivity.java
@@ -0,0 +1,23 @@
+package cn.studyjams.s2.sj175.jenice;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class AboutActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ActionBar actionbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        toolbar = (Toolbar) findViewById(R.id.about_toolbar);
+        setSupportActionBar(toolbar);
+        actionbar = getSupportActionBar();
+        actionbar.setSubtitle("关于Easy Mind");
+    }
+}
--- app/src/main/java/cn/studyjams/s2/sj175/jenice/MainActivity.java
+++ app/src/main/java/cn/studyjams/s2/sj175/jenice/MainActivity.java
@@ -1,40 +1,40 @@
 package cn.studyjams.s2.sj175.jenice;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+public class MainActivity extends AppCompatActivity{
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Toolbar toolbar;
 
-    private DatabaseReference mDatabase; // database
-
-    private TextView tvTest;
-    private Button btnTest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // To read data from the database, you need an instance of DatabaseReference
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+    }
 
-        tvTest= (TextView) findViewById(R.id.tv_test);
-        btnTest = (Button) findViewById(R.id.btn_test);
-
-        btnTest.setOnClickListener(this);
+    //toolbar右端
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btn_test:
-
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.option_1:
+                startActivity(new Intent(MainActivity.this,AboutActivity.class), ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
                 break;
         }
+        return true;
     }
 }

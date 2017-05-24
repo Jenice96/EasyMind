940eb7dd417f4ad0786fc119067298bb73b06929
Jenice96 <2861806421@qq.com>
2017-05-24 14:56:37 +0800

    add cardview
    完成从主界面到各个界面的跳转

---
+++ app/src/main/java/cn/studyjams/s2/sj175/jenice/CardView4Activity.java
@@ -0,0 +1,33 @@
+package cn.studyjams.s2.sj175.jenice;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class CardView4Activity extends AppCompatActivity implements View.OnClickListener{
+    private Toolbar toolbar;
+    private ImageView iv_1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_view4);
+
+        iv_1 = (ImageView) findViewById(R.id.iv_4);
+        toolbar = (Toolbar) findViewById(R.id.cardview4_toolbar);
+
+        setSupportActionBar(toolbar);
+        iv_1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.iv_4:
+                Toast.makeText(getApplicationContext(), "感谢阅读本文章，希望对您的学习有很大帮助！", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
--- app/src/main/java/cn/studyjams/s2/sj175/jenice/MainActivity.java
+++ app/src/main/java/cn/studyjams/s2/sj175/jenice/MainActivity.java
@@ -4,13 +4,16 @@ import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Toolbar toolbar;
+    private CardView cardView1,cardView2,cardView3,cardView4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,16 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
 
         toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        cardView1 = (CardView) findViewById(R.id.cardview1);
+        cardView2 = (CardView) findViewById(R.id.cardview2);
+        cardView3 = (CardView) findViewById(R.id.cardview3);
+        cardView4 = (CardView) findViewById(R.id.cardview4);
+
+        cardView1.setOnClickListener(this);
+        cardView2.setOnClickListener(this);
+        cardView3.setOnClickListener(this);
+        cardView4.setOnClickListener(this);
+
         setSupportActionBar(toolbar);
     }
 
@@ -36,5 +49,23 @@ public class MainActivity extends AppCompatActivity{
                 break;
         }
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.cardview1:
+                startActivity(new Intent(MainActivity.this,CardView1Activity.class), ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+                break;
+            case R.id.cardview2:
+                startActivity(new Intent(MainActivity.this,CardView2Activity.class), ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+                break;
+            case R.id.cardview3:
+                startActivity(new Intent(MainActivity.this,CardView3Activity.class), ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+                break;
+            case R.id.cardview4:
+                startActivity(new Intent(MainActivity.this,CardView4Activity.class), ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+                break;
+        }
     }
 }
